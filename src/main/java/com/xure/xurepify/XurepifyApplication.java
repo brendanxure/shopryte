700360165967fe2b44ba9ce0b010ba22d40a808d
brendanobilo <brendanxure@outlook.com>
2025-07-24 15:54:25 -0400

    first commit

---
+++ src/main/java/com/xure/xurepify/XurepifyApplication.java
@@ -0,0 +1,13 @@
+package com.xure.xurepify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XurepifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XurepifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xure/xurepify/XurepifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xure.xurepify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XurepifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
