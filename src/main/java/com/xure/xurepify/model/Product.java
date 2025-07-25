b365201d37292ef45accc90015715af06152cf0f
brendanobilo <brendanxure@outlook.com>
2025-07-25 17:03:31 -0400

    update

--- src/main/java/com/xure/xurepify/model/Product.java
+++ src/main/java/com/xure/xurepify/model/Product.java
@@ -1,4 +1,16 @@
 package com.xure.xurepify.model;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
 public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Double price;
+
+    private Category category;
 }
