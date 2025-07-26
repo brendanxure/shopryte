0a416f8e69e64c231aa33103cd9939d46558d3b8
brendanobilo <brendanxure@outlook.com>
2025-07-26 17:18:40 -0400

    update

--- src/main/java/com/xure/xurepify/model/Category.java
+++ src/main/java/com/xure/xurepify/model/Category.java
@@ -1,4 +1,19 @@
 package com.xure.xurepify.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.Set;
+
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products;
 }
--- src/main/java/com/xure/xurepify/model/Product.java
+++ src/main/java/com/xure/xurepify/model/Product.java
@@ -1,16 +1,20 @@
 package com.xure.xurepify.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
     private String imageUrl;
     private Double price;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
     private Category category;
 }
