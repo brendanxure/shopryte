9cb7fc7e6ca164e012aa6133dba967a71ebb25f3
brendanobilo <brendanxure@outlook.com>
2025-07-28 18:48:42 -0400

    update

--- src/main/java/com/xure/xurepify/controller/CategoryController.java
+++ src/main/java/com/xure/xurepify/controller/CategoryController.java
@@ -1,4 +1,14 @@
 package com.xure.xurepify.controller;
 
+import com.xure.xurepify.model.Category;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    public List<Category> getAllCategories;
 }
