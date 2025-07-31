ecc966afe84b090e37dedc45455e7f1b98660ebe
brendanobilo <brendanxure@outlook.com>
2025-07-31 18:59:09 -0400

    update

--- src/main/java/com/xure/xurepify/controller/CategoryController.java
+++ src/main/java/com/xure/xurepify/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.xure.xurepify.controller;
 
 import com.xure.xurepify.model.Category;
+import com.xure.xurepify.service.CategoryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
+    private CategoryService categoryService;
 
-    public List<Category> getAllCategories;
+    public List<Category> getAllCategories(){
+        categoryService.getAllCategories();
+    };
 }
--- src/main/java/com/xure/xurepify/service/CategoryService.java
+++ src/main/java/com/xure/xurepify/service/CategoryService.java
@@ -1,7 +1,13 @@
 package com.xure.xurepify.service;
 
+import com.xure.xurepify.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public void getAllCategories() {
+    }
 }
