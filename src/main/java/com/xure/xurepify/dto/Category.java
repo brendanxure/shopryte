7c6bc2ba4a1c108699c5ceb39da5aa1edc33c020
brendanobilo <brendanxure@outlook.com>
2025-08-22 21:03:04 -0400

    update

--- src/main/java/com/xure/xurepify/dto/Category.java
+++ src/main/java/com/xure/xurepify/dto/Category.java
@@ -1,9 +1,13 @@
 package com.xure.xurepify.dto;
 
+import com.xure.xurepify.model.Product;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class Category {
     private Long id;
     private String catName;
+    private Set<Product> catProducts;
 }
