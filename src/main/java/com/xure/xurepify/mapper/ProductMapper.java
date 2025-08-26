60c06d3e5833a6db9216c4c12cd83b22f06926e3
brendanobilo <brendanxure@outlook.com>
2025-08-25 21:33:05 -0400

    update

--- src/main/java/com/xure/xurepify/mapper/ProductMapper.java
+++ src/main/java/com/xure/xurepify/mapper/ProductMapper.java
@@ -1,4 +1,19 @@
 package com.xure.xurepify.mapper;
 
+import com.xure.xurepify.dto.ProductDto;
+import com.xure.xurepify.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
+    @Mapping(source = "name", target = "prodName")
+    @Mapping(source = "description", target = "prodDesc")
+    @Mapping(source = "category", target = "categoryDto")
+    ProductDto toDto(Product product);
+
+    @Mapping(source = "prodName", target = "name")
+    @Mapping(source = "prodDesc", target = "description")
+    @Mapping(source = "categoryDto", target = "category")
+    Product toEntity(ProductDto productDto);
 }
